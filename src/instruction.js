6058e8017be375b6b3d7b9681edb0df177b44467
law-wang <47406212+law-wang@users.noreply.github.com>
2021-03-09 18:32:55 -0500

    notes

--- src/instruction.js
+++ src/instruction.js
@@ -7,7 +7,7 @@ export default () => {
         <section id="instruction-left">
           <div style={{fontSize: "40px", marginBottom: "5px", fontWeight: "700"}}>Access any Spotify playlist using its playlist ID or link</div>
           <div>For a copy-and-pastable list of track, artist, and album names.</div>
-          <div style={{fontSize: "30px", fontWeight: "700", marginTop: "30px", marginBottom: "5px", fontStyle: "italic"}}>Something to get started with?</div>
+          <div style={{fontSize: "30px", fontWeight: "700", marginTop: "30px", marginBottom: "5px", fontStyle: "italic"}}>Try these playlists?</div>
           <table>
             <tbody>
               <tr>
@@ -28,9 +28,10 @@ export default () => {
         <section id="instruction-right" style={{fontSize: "15px"}}>
           <div style={{fontSize: "20px", marginBottom: "10px"}}>Spotlist is a <a href="https://github.com/law-wang/spotlist">project</a> by <a href="https://www.rence.la/">Lawrence</a> using React and Spotify API.</div>
           <div><b>2020.08.02:</b> initial launch.</div>
-          <div><b>2020.08.16:</b> neumorphic style update.</div>
-          <div><b>2021.01.22:</b> table style overhaul.</div>
-          <div><b>2021.01.23:</b> home page style overhaul; random background color; playlist content links to Spotify; search with playlist link in addition to ID. </div>
+          <div><b>2020.08.16:</b> style update: neumorphic style update.</div>
+          <div><b>2021.01.22:</b> style update: table style overhaul.</div>
+          <div><b>2021.01.23:</b> style update: home page overhaul, random background color; feature update: playlist content links to Spotify, search with playlist link in addition to ID. </div>
+          <div><b>2021.03.09:</b> bug fix: ability to get tracks from multi-page responses. </div>
         </section>
         </div>
     )
